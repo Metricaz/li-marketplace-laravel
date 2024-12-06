da4fb007e04ed803788bf1bae230b03bb4d5b92b
Bernardo Ribeiro Generozo <bernardo.generozo@metricaz.com>
2024-12-06 13:23:19 -0300

    Added category text

--- resources/views/newlayout/category/partials/category-description-bottom.blade.php
+++ resources/views/newlayout/category/partials/category-description-bottom.blade.php
@@ -2,7 +2,7 @@
   <div class="o-container">
     <h2 class="c-category-description-bottom__title">
       @if ($categoryText)
-        {{ $categoryText->bottom_text_tittle }}
+        {{ $categoryText->bottom_text_title }}
       @else
         Lorem ipsum dolor sit amet
       @endif
@@ -11,7 +11,7 @@
     <div class="c-category-description-bottom__text">
       <p>
       @if ($categoryText)
-        {{ $categoryText->bottom_text }}
+        {!! $categoryText->bottom_text !!}
       @else
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consequat a nulla vitae pellentesque. Nunc ac cursus purus, quis posuere erat. Phasellus nulla quam, sagittis id nisi sed, tristique faucibus ante. Donec sed ultrices diam, vitae tristique nisl. Nulla imperdiet purus eu consectetur fermentum. Curabitur faucibus, massa eu aliquam sollicitudin, nulla mi lobortis eros, nec pretium ante mi non quam. Vestibulum eget ultrices mi. Aenean consectetur mollis erat, in finibus sem volutpat a. Cras sit amet ultrices ligula, id blandit tellus. Maecenas condimentum imperdiet mauris eu bibendum. Nunc maximus lorem id sem ullamcorper tincidunt. Aenean venenatis arcu ac aliquet tristique. Sed dapibus felis a risus fermentum hendrerit. Morbi a eros metus. Nulla consequat, massa ut ullamcorper facilisis, nulla nulla molestie purus, eu consequat velit neque ac diam.
       @endif
