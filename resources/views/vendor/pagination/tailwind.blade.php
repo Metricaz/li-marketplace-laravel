407131de5930ffe5b888439be51705b55f16fbf4
Bernardo Ribeiro Generozo <bernardo.generozo@metricaz.com>
2024-12-13 15:42:33 -0300

    Added text api call

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -3,54 +3,52 @@
         <div class="flex justify-between flex-1 sm:hidden pagination_bc">
             @if ($paginator->onFirstPage())
                 <span class="no_pagination_bc">
-                    Anterior
+                    <
                 </span>
             @else
                 <a href="{{ $paginator->previousPageUrl() }}" class="">
-                    Anterior
+                    <
                 </a>
             @endif
+            <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between pagination_bc numbers_bc">
+                <div class="arrows-pagination">
+                    <span class="relative z-0 inline-flex rtl:flex-row-reverse shadow-sm rounded-md">
+                        {{-- Pagination Elements --}}
+                        @foreach ($elements as $element)
+                            {{-- "Three Dots" Separator --}}
+                            @if (is_string($element))
+                                <span aria-disabled="true">
+                                    <span class="">{{ $element }}</span>
+                                </span>
+                            @endif
 
+                            {{-- Array Of Links --}}
+                            @if (is_array($element))
+                                @foreach ($element as $page => $url)
+                                    @if ($page == $paginator->currentPage())
+                                        <span aria-current="page">
+                                            <span class="active_pagination_bc">{{ $page }}</span>
+                                        </span>
+                                    @else
+                                        <a href="{{ $url }}" class="" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
+                                            {{ $page }}
+                                        </a>
+                                    @endif
+                                @endforeach
+                            @endif
+                        @endforeach
+                    </span>
+                </div>
+            </div>
             @if ($paginator->hasMorePages())
                 <a href="{{ $paginator->nextPageUrl() }}" class="">
-                    Próximo
+                    >
                 </a>
             @else
                 <span class="no_pagination_bc">
-                    Próximo
+                    >
                 </span>
             @endif
-        </div>
-
-        <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between pagination_bc numbers_bc">
-            <div class="arrows-pagination">
-                <span class="relative z-0 inline-flex rtl:flex-row-reverse shadow-sm rounded-md">
-                    {{-- Pagination Elements --}}
-                    @foreach ($elements as $element)
-                        {{-- "Three Dots" Separator --}}
-                        @if (is_string($element))
-                            <span aria-disabled="true">
-                                <span class="">{{ $element }}</span>
-                            </span>
-                        @endif
-
-                        {{-- Array Of Links --}}
-                        @if (is_array($element))
-                            @foreach ($element as $page => $url)
-                                @if ($page == $paginator->currentPage())
-                                    <span aria-current="page">
-                                        <span class="active_pagination_bc">{{ $page }}</span>
-                                    </span>
-                                @else
-                                    <a href="{{ $url }}" class="" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
-                                        {{ $page }}
-                                    </a>
-                                @endif
-                            @endforeach
-                        @endif
-                    @endforeach
-                </span>
-            </div>
         </div>
     </nav>
 @endif
