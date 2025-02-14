dcc8eb394545820f671320662375ea95e40233d4
Generozo <bernardo.generozo@metricaz.com>
2025-02-14 12:13:03 -0300

    fixed css

--- resources/views/newlayout/partials/product-card-mini.blade.php
+++ resources/views/newlayout/partials/product-card-mini.blade.php
@@ -1,7 +1,7 @@
-<div class="c-product-card-mini">
-  <a
-    href="{{ $url ?? '' }}"
-  >
+<a
+  href="{{ $url ?? '' }}"
+  class="c-product-card-mini"
+>
   <img
     class="c-product-card-mini__img"
     src="{{ $image_url ?? '' }}"
@@ -10,26 +10,21 @@
     height="222"
   >
 
-    <div class="c-product-card-mini__body">
-      <h3 class="c-product-card-mini__title">
-        {{ $title ?? '' }}
-      </h3>
+  <div class="c-product-card-mini__body">
+    <h3 class="c-product-card-mini__title">
+      {{ $title ?? '' }}
+    </h3>
 
-      <div class="c-product-card-mini__price">
-        @if (isset($old_price))
-          <span class="c-product-card-mini__old-price">R$ {{ $old_price }}</span>
-        @endif
+    <div class="c-product-card-mini__price">
+      @if (isset($old_price))
+        <span class="c-product-card-mini__old-price">R$ {{ $old_price }}</span>
+      @endif
 
-        <span class="c-product-card-mini__new-price">R$ {{ $price ?? '' }}</span>
-      </div>
+      <span class="c-product-card-mini__new-price">R$ {{ $price ?? '' }}</span>
     </div>
-  </a>
 
-  <div>
-    <a href="{{ $product->offer_link ?? '' }}">
-      <div class="c-product-card-mini__button">
-        Ver <strong>Oferta</strong> @include('newlayout.partials.svg.icons.external-link')
-      </div>
-    </a>
+    <div class="c-product-card-mini__button">
+      Ver <strong>Oferta</strong> @include('newlayout.partials.svg.icons.external-link')
+    </div>
   </div>
-</div>
+</a>
