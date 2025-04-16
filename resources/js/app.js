57ca7cd8296e4e6e5ea5d4ff964c022364289c05
Generozo <bernardo.generozo@metricaz.com>
2025-04-16 16:14:03 -0300

    added admin to marketplace

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,39 +1,7 @@
 import './bootstrap';
 
+import Alpine from 'alpinejs';
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const checkbox = document.getElementById('myCheckbox');
+window.Alpine = Alpine;
 
-    checkbox.addEventListener('change', () => {
-        const isChecked = checkbox.checked;
-
-        // Data to send to the controller
-        const data = { checked: isChecked };
-
-        // Create an XMLHttpRequest object
-        const xhr = new XMLHttpRequest();
-        
-        // Define what happens on successful data submission
-        xhr.onload = function () {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                console.log('Success:', xhr.responseText);
-            } else {
-                console.log('The request failed!');
-            }
-        };
-        
-        // Define what happens in case of error
-        xhr.onerror = function () {
-            console.log('There was an error!');
-        };
-        
-        // Set up the request with the HTTP method and the URL to send the data to
-        xhr.open('POST', '/controller-endpoint');
-        
-        // Set the request header to let the server know the type of content being sent
-        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-        
-        // Send the request with the data as a JSON string
-        xhr.send(JSON.stringify(data));
-    });
-});
+Alpine.start();
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AboutUsController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HealthCheckController;
@@ -26,27 +27,27 @@ use Illuminate\Support\Facades\Route;
  * Rotas teste para desenvolvimento dos layouts
  */
 Route::get('/newlayout', function() {
-	return view('newlayout.home.index');
+    return view('newlayout.home.index');
 });
 
 Route::get('/newlayout/categoria', function() {
-	return view('newlayout.category.index');
+    return view('newlayout.category.index');
 });
 
 Route::get('/newlayout/produto', function() {
-	return view('newlayout.product.index');
+    return view('newlayout.product.index');
 });
 
 Route::get('/termos-de-uso', function() {
-	return view('newlayout.pages.terms-of-use');
+    return view('newlayout.pages.terms-of-use');
 });
 
 Route::get('/politica-e-privacidade', function() {
-	return view('newlayout.pages.policy-and-privacy');
+    return view('newlayout.pages.policy-and-privacy');
 });
 
 Route::get('/quem-somos', function() {
-	return view('newlayout.pages.about-us');
+    return view('newlayout.pages.about-us');
 });
 
 Route::get('/sitemap.xml', [SitemapController::class, 'index'])->name('sitemap');
@@ -62,3 +63,15 @@ Route::get('s/{search}/', [SearchController::class, 'index'])->name('search');
 Route::get('/{sku}/p', [ProductPageController::class, 'show']);
 
 Route::get('health-check', [HealthCheckController::class, 'check']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
